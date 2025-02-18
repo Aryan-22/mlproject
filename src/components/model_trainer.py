14d4a2fb10bca1bf1cf5e4988c6eaf22942e9aea
Aryan Tarafdar <aryancoder03@gmail.com>
2025-02-18 23:28:37 +0530

    Hyperparameter tuning done

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -36,13 +36,46 @@ class ModelTrainer:
             models = {
             "Linear Regression": LinearRegression(),
             "Gradient Boosting":GradientBoostingRegressor(),
-            "K-Neighbors Regressor": KNeighborsRegressor(),
             "Decision Tree": DecisionTreeRegressor(),
             "Random Forest Regressor": RandomForestRegressor(),
             "XGBRegressor": XGBRegressor(), 
             "AdaBoost Regressor": AdaBoostRegressor()
              }
-            model_report = evaluate_model(X_train, y_train, X_test, y_test,models)
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest Regressor":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001], 
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
+
+            model_report = evaluate_model(X_train, y_train, X_test, y_test,models,params)
 
             best_model_score = max(list(model_report.values()))
             best_model_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)]
